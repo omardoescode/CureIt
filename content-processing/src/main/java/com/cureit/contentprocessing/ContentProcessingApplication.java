043a83ce6649a7a99f5b32f5ab7fd479a282554a
Roaa Mohamed <roaaamohamed66@gmail.com>
2025-11-01 13:35:12 +0200

    init: content processing service structure

---
+++ content-processing/src/main/java/com/cureit/contentprocessing/ContentProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.cureit.contentprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentProcessingApplication.class, args);
+    }
+
+}
---
+++ content-processing/src/test/java/com/cureit/contentprocessing/ContentProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cureit.contentprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
