7c1b2480b523c656e315911c4f8147726ba7932d
omardoescode <omardoescode@gmail.com>
2025-11-04 08:09:08 +0200

    feat(content-storage): Add submission information in content-storage

--- content_storage/src/service/ContentItem.ts
+++ content_storage/src/service/ContentItem.ts
@@ -1,3 +1,4 @@
+import ContentItemSubmission from "@/models/ContentItemSubmission";
 import type { ContentSubmissionBody } from "../validation/content";
 import type { BaseHeaders } from "../validation/headers";
 import ContentItem from "@/models/ContentItem";
@@ -12,20 +13,33 @@ export async function submitContent(
     source_url,
     type,
     markdown,
-    // submitted_at,
-    // topics
+    submitted_at,
+    topics,
   }: ContentSubmissionBody,
 ): Promise<string> {
-  const cnt = new ContentItem({
-    author,
-    title,
-    extracted_at,
-    is_private,
-    source_url,
-    type,
-    markdown,
+  let content = await ContentItem.findOne({ source_url });
+
+  if (!content) {
+    content = new ContentItem({
+      author,
+      title,
+      extracted_at,
+      is_private,
+      source_url,
+      type,
+      markdown,
+    });
+    await content.save();
+  }
+
+  // Add a submission
+  const submission = new ContentItemSubmission({
+    content_id: content._id,
+    user_id: headers["CureIt-User-Id"],
+    submitted_at,
+    topics,
   });
 
-  await cnt.save();
-  return cnt._id;
+  await submission.save();
+  return content._id;
 }
