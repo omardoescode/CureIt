fd7601765c1c545eba73a147f3ed4373064f7a63
omardoescode <omardoescode@gmail.com>
2025-11-04 08:34:17 +0200

    refactor: add hono/logger to api_gateway

--- api_gateway/src/index.ts
+++ api_gateway/src/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
 import v1app from "./v1";
 import env from "./env";
+import { logger as logMiddlware } from "hono/logger";
 
 const app = new Hono().basePath("/api");
+app.use(logMiddlware());
 
 app.route("/v1/", v1app);
 
