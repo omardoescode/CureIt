174d5abb56b0cfc02466958e9efa1f74b5733861
omardoescode <omardoescode@gmail.com>
2025-11-24 15:39:47 +0200

    feat: Migrate to using uuid v7 as primary keys

--- user-service/src/main/java/com/Curelt/user_service/repository/FileRepository.java
+++ user-service/src/main/java/com/Curelt/user_service/repository/FileRepository.java
@@ -4,6 +4,8 @@ import com.Curelt.user_service.entities.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface FileRepository extends JpaRepository<File,Long> {
+public interface FileRepository extends JpaRepository<File, UUID> {
 }
