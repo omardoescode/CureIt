d2d44d7334a45518b10108e6975d38b43649951c
omardoescode <omardoescode@gmail.com>
2025-12-14 20:46:02 +0200

    feat: add an api gateway endpoint for retrieving content

--- api-gateway/src/v1/index.ts
+++ api-gateway/src/v1/index.ts
@@ -1,9 +1,8 @@
 import env from "@/env";
-import { Hono, type Context } from "hono";
+import { Hono } from "hono";
 import { type StatusCode } from "hono/utils/http-status";
 import logger from "@/lib/logger";
 import { decode } from "hono/jwt";
-import type { SafeExtendShape } from "zod";
 
 const v1app = new Hono();
 
@@ -85,6 +84,33 @@ v1app.post("/content/submit", async (c) => {
           ...body,
           submitted_at: new Date().toISOString(),
         }),
+      },
+    );
+
+    c.status(res.status as StatusCode);
+    res.headers.forEach((value, key) => c.header(key, value));
+    const responseBody = await res.arrayBuffer();
+    return c.body(responseBody);
+  } catch (err) {
+    logger.error("Failed to submit content", { error: err });
+    return c.json({ error: "Internal Server Error" }, 500);
+  }
+});
+
+v1app.get("/content/:slug", async (c) => {
+  try {
+    const slug = c.req.param("slug");
+    const userId = await getUserId(c.req.header("Authorization"));
+    if (!userId) return c.json({ error: "Unauthorized" }, 401);
+
+    const res = await fetch(
+      `${env.CONTENT_STORAGE_SERVICE_URL}/api/content/${slug}`,
+      {
+        method: "GET",
+        headers: {
+          "CureIt-Coordination-Id": crypto.randomUUID(),
+          "CureIt-User-Id": userId,
+        },
       },
     );
 
