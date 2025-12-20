0eff73216a0eede8e1a9b75aca72c75ddd6dda94
omardoescode <omardoescode@gmail.com>
2025-12-20 12:46:31 +0200

    feat: Put the interaction interface in the api gateway

--- api-gateway/src/v1/index.ts
+++ api-gateway/src/v1/index.ts
@@ -1,16 +1,14 @@
-import env from "@/env";
 import { Hono } from "hono";
-import { type StatusCode } from "hono/utils/http-status";
-import logger from "@/lib/logger";
-import { getUserId } from "./helpers";
 import FeedRouter from "./FeedRouter";
 import AuthRouter from "./AuthRouter";
 import ContentRouter from "./ContentRouter";
+import InteractionRouter from "./InteractionRouter";
 
 const v1app = new Hono();
 
 v1app.route("/feed", FeedRouter);
 v1app.route("/auth", AuthRouter);
 v1app.route("/content", ContentRouter);
+v1app.route("/interaction", InteractionRouter);
 
 export default v1app;
